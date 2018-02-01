cfc6c67a0d9d5687f9502af64f8342c03bbd78a9
Josh Godinez <joshua.e.godinez@gmail.com>
2018-01-31 19:57:40 -0600

    add footer

---
+++ instagram-web/src/Components/Home/Home.js
@@ -0,0 +1,21 @@
+import React , { Component } from 'react';
+import Footer from './Footer/Footer';
+import './.css';
+ 
+class Home extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+ 
+    }
+  }
+  render(){
+    return (
+      <div className="">
+        
+        <Footer/>
+      </div>
+    )
+  }
+}
+export default Home;
--- instagram-web/src/Ducks/reducer.js
+++ instagram-web/src/Ducks/reducer.js
@@ -7,64 +7,64 @@ const initialState = {
  
  function reducer( state = initialState, action ) { 
      switch( action.type ){
-       case UPDATE_LOAN_TYPE:
-         return Object.assign( {}, state, { loanType: action.payload } );
+      //  case UPDATE_LOAN_TYPE:
+      //    return Object.assign( {}, state, { loanType: action.payload } );
  
-       case UPDATE_PROPERTY_TYPE:
-         return Object.assign( {}, state, { propertyType: action.payload } );
+      //  case UPDATE_PROPERTY_TYPE:
+      //    return Object.assign( {}, state, { propertyType: action.payload } );
  
-       case UPDATE_CITY:
-         return Object.assign( {}, state, { city: action.payload } );
+      //  case UPDATE_CITY:
+      //    return Object.assign( {}, state, { city: action.payload } );
         
-       case `${SNAKE_CASE}_PENDING`:
-         return Object.assign({}, state, { isLoading: true });
+      //  case `${SNAKE_CASE}_PENDING`:
+      //    return Object.assign({}, state, { isLoading: true });
    
-       case `${SNAKE_CASE}_FULFILLED`:
-         return Object.assign({}, state, {
-           isLoading: false,
-           user: action.payload,
-           isAuthenticated: true
-         });
+      //  case `${SNAKE_CASE}_FULFILLED`:
+      //    return Object.assign({}, state, {
+      //      isLoading: false,
+      //      user: action.payload,
+      //      isAuthenticated: true
+      //    });
    
-       case `${SNAKE_CASE}_REJECTED`:
-         return Object.assign({}, state, {
-           isLoading: false,
-           didError: true
-         });
+      //  case `${SNAKE_CASE}_REJECTED`:
+      //    return Object.assign({}, state, {
+      //      isLoading: false,
+      //      didError: true
+      //    });
 
        default: return state;
      }
  } 
  
- export function register(google_id) {
-    return {
-      type: SNAKE_CASE,
-      payload: axios
-        .post("/api/register", { google_id })
-        .then(response => response.data[0])
-        .catch(console.log)
-    };
-  }
+//  export function register(google_id) {
+//     return {
+//       type: SNAKE_CASE,
+//       payload: axios
+//         .post("/api/register", { google_id })
+//         .then(response => response.data[0])
+//         .catch(console.log)
+//     };
+//   }
 
- export function updateLoanType( loanType ){
-   return {
-     type: UPDATE_LOAN_TYPE,
-     payload: loanType
-   };
- }
+//  export function updateLoanType( loanType ){
+//    return {
+//      type: UPDATE_LOAN_TYPE,
+//      payload: loanType
+//    };
+//  }
  
- export function updatePropertyType( property ) {
-   return {
-     type: UPDATE_PROPERTY_TYPE,
-     payload: property
-   };
- }
+//  export function updatePropertyType( property ) {
+//    return {
+//      type: UPDATE_PROPERTY_TYPE,
+//      payload: property
+//    };
+//  }
  
- export function updateCity( city ) {
-   return {
-     type: UPDATE_CITY,
-     payload: city
-   };
- }
+//  export function updateCity( city ) {
+//    return {
+//      type: UPDATE_CITY,
+//      payload: city
+//    };
+//  }
  
  export default reducer; 
