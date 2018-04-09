84559d7d6a32f32aa965e5342ee816ac07ca544a
Bruno Carvalho <dipsnatchy26@gmail.com>
2018-04-09 20:27:27 +0100

    finish bottom nav

--- instagram-app/src/components/BottomNav/BottomNav.js
+++ instagram-app/src/components/BottomNav/BottomNav.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { TouchableWithoutFeedback, View, Image } from "react-native";
 import { styles } from "./BottomNavStyles";
+import { Divider } from "react-native-elements";
 
-import homeIcon from "./../../assets/images/homeIcon.png";
+import homeIconActive from "./../../assets/images/homeIconActive.png";
 import searchIcon from "./../../assets/images/searchIcon.png";
 import createIcon from "./../../assets/images/createIcon.png";
 import activityIcon from "./../../assets/images/activityIcon.png";
@@ -11,31 +12,34 @@ import profileIcon from "./../../assets/images/profileIcon.png";
 class BottomNav extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <View>
-          <TouchableWithoutFeedback>
-            <Image source={homeIcon} style={styles.icons} />
-          </TouchableWithoutFeedback>
-        </View>
-        <View>
-          <TouchableWithoutFeedback>
-            <Image source={searchIcon} style={styles.icons} />
-          </TouchableWithoutFeedback>
-        </View>
-        <View>
-          <TouchableWithoutFeedback>
-            <Image source={createIcon} style={styles.icons} />
-          </TouchableWithoutFeedback>
-        </View>
-        <View>
-          <TouchableWithoutFeedback>
-            <Image source={activityIcon} style={styles.icons} />
-          </TouchableWithoutFeedback>
-        </View>
-        <View>
-          <TouchableWithoutFeedback>
-            <Image source={profileIcon} style={styles.icons} />
-          </TouchableWithoutFeedback>
+      <View>
+        <Divider style={styles.divider} />
+        <View style={styles.container}>
+          <View>
+            <TouchableWithoutFeedback>
+              <Image source={homeIconActive} style={styles.icons} />
+            </TouchableWithoutFeedback>
+          </View>
+          <View>
+            <TouchableWithoutFeedback>
+              <Image source={searchIcon} style={styles.icons} />
+            </TouchableWithoutFeedback>
+          </View>
+          <View>
+            <TouchableWithoutFeedback>
+              <Image source={createIcon} style={styles.icons} />
+            </TouchableWithoutFeedback>
+          </View>
+          <View>
+            <TouchableWithoutFeedback>
+              <Image source={activityIcon} style={styles.icons} />
+            </TouchableWithoutFeedback>
+          </View>
+          <View>
+            <TouchableWithoutFeedback>
+              <Image source={profileIcon} style={styles.icons} />
+            </TouchableWithoutFeedback>
+          </View>
         </View>
       </View>
     );
